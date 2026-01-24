5d4949f0ca0b1f1de4a8a7ec40de568ccc74fe83
ZhouHuixing <2351289@tongji.edu.cn>
2026-01-24 13:24:09 +0800

    删除docker

--- restart.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "正在重启服务..." -ForegroundColor Cyan
-docker-compose restart
-Write-Host "服务已重启" -ForegroundColor Cyan
-Write-Host "前端: http://localhost" -ForegroundColor Cyan
-Write-Host "后端: http://localhost:8080/docs" -ForegroundColor Cyan
